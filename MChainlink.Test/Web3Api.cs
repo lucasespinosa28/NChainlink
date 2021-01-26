cacef7ef681d8b7b31649f20b517b2f51344d753
lucas espinosa <lucasespinosa159@gmail.com>
2021-01-26 09:24:02 -0300

    unused dependency removed

--- MChainlink.Test/Web3Api.cs
+++ MChainlink.Test/Web3Api.cs
@@ -4,7 +4,7 @@ namespace MChainlink.Test
 {
     public static class Web3Api
     {
-        public static Web3 Web => new Web3("https://mainnet.infura.io/v3/1cca02da44c94251b71d81d05eabe285");
+        public static Web3 Web => new Web3("https://mainnet.infura.io/v3/84d53bc74e0845119b4c34c4657e89e9");
         public static string Address => "0xDC530D9457755926550b59e8ECcdaE7624181557";
     }
     
